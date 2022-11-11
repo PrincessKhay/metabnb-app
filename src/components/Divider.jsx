e79d6bb8790ba29cc4a1548f1fd29175866a96e7
PrincessKhay <olusegunprincess8@gmail.com>
2022-11-11 22:43:31 +0100

    style: added styling to the component

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -3,19 +3,19 @@ import React from "react";
 function Divider () {
     return(
         <div className=" bg-primary ">
-            <div className="mx-28 flex justify-between">
+            <div className="mx-28 py-2 flex justify-between">
 
-              <div className="flex items-center">
-                <img src="images/mb-token.svg" alt="" />
-                <p className="text-white">MBToken</p>
+              <div className="w-52 flex items-center">
+                <img src="images/mb-token.svg" alt="" className="w-8"/>
+                <p className="text-token text-2xl font-bold tracking-wide">MBToken</p>
               </div>
-              <div className="flex items-center">
-                <img src="images/white-metamask.svg" alt=""/>
-                <p className="text-white">METAMASK</p>
+              <div className="w-52 flex items-center space-x-2">
+                <img src="images/white-metamask.svg" alt="" className="w-7"/>
+                <p className="text-token text-xl tracking-wider">METAMASK</p>
               </div>
-              <div className="flex items-center">
-                <img src="images/opensea.svg" alt=""/>
-                <p className="text-white">OpenSea</p>
+              <div className="w-52 flex items-center space-x-2">
+                <img src="images/opensea.svg" alt="" className="w-7"/>
+                <p className="text-white text-2xl font-bold tracking-tight">OpenSea</p>
               </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
         bordercolor: "#a3a3a3",
         input: "rgba(247, 247, 247, 0.1)",
         placeholdertext: "#b8b8b8",
+        token: "#f7f7f7",
       }
     },
   },
