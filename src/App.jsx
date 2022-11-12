2504858fa7bd1cee0406a8a36f39e9867186bb48
PrincessKhay <olusegunprincess8@gmail.com>
2022-11-12 17:07:48 +0100

    changed card collection style to mapping

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Divider from "./components/Divider";
 import Collection from "./components/Collection";
 import Moreinfo from "./components/Moreinfo";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
     <Divider/>
     <Collection />
     <Moreinfo/>
+    <Footer/>
     </div>
   );
 }
--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,35 +1,27 @@
 import React from "react";
 import Card from "./Card";
 
+
+const images = [
+    "images/card-1.svg",
+    "images/card-2.svg",
+    "images/card-3.svg",
+    "images/card-4.svg",
+    "images/card-5.svg",
+    "images/card-6.svg",
+    "images/card-7.svg",
+    "images/card-8.svg",
+]
+
 function Collection () {
     return (
         <div className="mx-20 my-14">
             <h1 className="text-4xl font-bold text-center">Inspiration for your next adventure</h1>
             <div className="grid grid-cols-4 gap-6 mt-10">
-                    <Card 
-                        img="images/card-1.svg"
-                    />
-                    <Card 
-                        img="images/card-2.svg"
-                    />
-                    <Card 
-                        img="images/card-3.svg"
-                    />
-                    <Card 
-                        img="images/card-4.svg"
-                    />
-                    <Card 
-                        img="images/card-5.svg"
-                    />
-                    <Card 
-                        img="images/card-6.svg"
-                    />
-                    <Card 
-                        img="images/card-7.svg"
-                    />
-                    <Card 
-                        img="images/card-8.svg"
-                    />
+            {images.map((image)=>{
+                return <Card img={image}/>
+            })}
+                    
             </div>
         </div>
     )
