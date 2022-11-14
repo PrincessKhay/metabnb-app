0e4fb5838ac17b628397e41f1eeb3c5a6fd058ad
PrincessKhay <olusegunprincess8@gmail.com>
2022-11-14 01:27:32 +0100

    made the component responsive

--- src/components/homeComponents/Divider.jsx
+++ src/components/homeComponents/Divider.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 
-function Divider () {
-    return(
-        <div className=" bg-primary ">
-            <div className="mx-28 py-2 flex justify-between">
-
-              <div className="w-52 flex items-center">
-                <img src="images/mb-token.svg" alt="" className="w-8"/>
-                <p className="text-token text-2xl font-bold tracking-wide">MBToken</p>
-              </div>
-              <div className="w-52 flex items-center space-x-2">
-                <img src="images/white-metamask.svg" alt="" className="w-7"/>
-                <p className="text-token text-xl tracking-wider">METAMASK</p>
-              </div>
-              <div className="w-52 flex items-center space-x-2">
-                <img src="images/opensea.svg" alt="" className="w-7"/>
-                <p className="text-white text-2xl font-bold tracking-tight">OpenSea</p>
-              </div>
-            </div>
+function Divider() {
+  return (
+    <div className=" bg-primary ">
+      <div className="mx-28 py-2 flex justify-between">
+        <div className="md:w-52 mdx:w-24 flex items-center">
+          <img src="images/mb-token.svg" alt="" className="w-8" />
+          <p className="text-token text-2xl font-bold tracking-wide">MBToken</p>
         </div>
-    );
+        <div className="md:w-52 mdx:w-24 flex items-center space-x-2">
+          <img src="images/white-metamask.svg" alt="" className="w-7" />
+          <p className="text-token text-xl tracking-wider">METAMASK</p>
+        </div>
+        <div className="md:w-52 mdx:w-24 flex items-center space-x-2">
+          <img src="images/opensea.svg" alt="" className="w-7" />
+          <p className="text-white text-2xl font-bold tracking-tight">
+            OpenSea
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Divider;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    // screens: {
-    //   "2xlx": { max: "1535px" },
-    //   // => @media (max-width: 1535px) { ... }
-
-    //   xlx: { max: "1279px" },
-    //   // => @media (max-width: 1279px) { ... }
-
-    //   lgx: { max: "1023px" },
-    //   // => @media (max-width: 1023px) { ... }
-
-    //   mdx: { max: "767px" },
-    //   // => @media (max-width: 767px) { ... }
-
-    //   smx: { max: "639px" },
-    //   // => @media (max-width: 639px) { ... }
-
-    //   xsx: { max: "425px" },
-    // },
     extend: {
+      screens: {
+        "2xlx": { max: "1535px" },
+        // => @media (max-width: 1535px) { ... }
+
+        xlx: { max: "1279px" },
+        // => @media (max-width: 1279px) { ... }
+
+        lgx: { max: "1023px" },
+        // => @media (max-width: 1023px) { ... }
+
+        mdx: { max: "767px" },
+        // => @media (max-width: 767px) { ... }
+
+        smx: { max: "639px" },
+        // => @media (max-width: 639px) { ... }
+
+        xsx: { max: "425px" },
+      },
       colors: {
         primary: "#a02279",
         bordercolor: "#a3a3a3",
