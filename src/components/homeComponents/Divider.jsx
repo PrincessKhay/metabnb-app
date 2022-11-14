4432c715ec6a0a6696e9dac18aeb97c46461e80c
PrincessKhay <olusegunprincess8@gmail.com>
2022-11-14 19:24:11 +0100

    adjuested component for responsiveness

--- src/components/homeComponents/Divider.jsx
+++ src/components/homeComponents/Divider.jsx
@@ -3,18 +3,22 @@ import React from "react";
 function Divider() {
   return (
     <div className=" bg-primary ">
-      <div className="mx-28 py-2 flex justify-between">
+      <div className="xsx:mx-8 mx-28 py-2 flex justify-between">
         <div className="md:w-52 mdx:w-24 flex items-center">
           <img src="images/mb-token.svg" alt="" className="w-8" />
-          <p className="text-token text-2xl font-bold tracking-wide">MBToken</p>
+          <p className="text-token xsx:text-xl text-2xl font-bold tracking-wide">
+            MBToken
+          </p>
         </div>
         <div className="md:w-52 mdx:w-24 flex items-center space-x-2">
           <img src="images/white-metamask.svg" alt="" className="w-7" />
-          <p className="text-token text-xl tracking-wider">METAMASK</p>
+          <p className="text-token xsx:text-base text-xl tracking-wider">
+            METAMASK
+          </p>
         </div>
         <div className="md:w-52 mdx:w-24 flex items-center space-x-2">
           <img src="images/opensea.svg" alt="" className="w-7" />
-          <p className="text-white text-2xl font-bold tracking-tight">
+          <p className="text-white xsx:text-xl text-2xl font-bold tracking-tight">
             OpenSea
           </p>
         </div>
