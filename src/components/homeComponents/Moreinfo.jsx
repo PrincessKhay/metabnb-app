5d42dd945ef5cf316a11cdcbf9195f52d93847c1
Olusegun Princess Kemi <olusegunprincess8@gmail.com>
2022-11-16 13:27:09 +0100

    adjusted the spacing

--- src/components/homeComponents/Moreinfo.jsx
+++ src/components/homeComponents/Moreinfo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Moreinfo() {
   return (
     <div className="bg-primary pl-20 pt-28 pb-20 grid grid-cols-2 llgx:grid-cols-1 smx:flex smx:flex-col smx:px-14 smx:items-center xsx:px-8 xsx:pt-14  justify-between">
-      <div className="space-y-12 pr-52 mt-10 smx:p-0 smx:w-full smx:m-0 xsx:flex xsx:flex-col">
+      <div className="space-y-12 pr-52 mt-10 smx:p-0 smx:w-full smx:m-0 xsx:flex xsx:flex-col xlx2:pr-20">
         <h1 className="text-4xl font-bold text-white smx:text-left">
           Metabnb NFTs
         </h1>
@@ -16,7 +16,7 @@ function Moreinfo() {
           Learn more
         </button>
       </div>
-      <div className="w-8/12 pl-10 smx:pl-0 smx:w-full smx:mt-10 smx:mb-4 xsx:mx-auto">
+      <div className="w-8/12 pl-10 smx:pl-0 smx:w-full smx:mt-10 smx:mb-4 xsx:mx-auto xlx2:w-full xlx2:pr-16">
         <img src="images/3-images.svg" alt="nft-images" />
       </div>
       <div className="sm:hidden">
